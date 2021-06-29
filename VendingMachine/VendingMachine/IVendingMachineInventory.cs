0314480ad6cd4571e87ef841a107c82047ac9fb4
yusuf <yusuf.eroglu@cresaurteam.com>
2021-06-30 02:11:05 +0300

    add inventory

---
+++ VendingMachine/VendingMachine/IVendingMachineInventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public interface IVendingMachineInventory
+    {
+        void SetUpInventory(Catalogue catalogue);
+        void AddItem(VMItem item);
+        void AddItem(List<VMItem> items);
+    }
+}
---
+++ VendingMachine/VendingMachine/VMInventory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public class VMInventory :IVendingMachineInventory
+    {
+        public List<Drink> DrinkItems { get; set; }
+        public List<Food> FoodItems { get; set; }
+        public List<Weapon> WeaponItems { get; set; }
+
+        public void SetUpInventory(Catalogue catalogue)
+        {
+            DrinkItems = catalogue.DrinkItems;
+            FoodItems = catalogue.FoodItems;
+            WeaponItems = catalogue.WeaponItems;
+        }
+
+        public void AddItem(VMItem item)
+        {
+            switch (item.Type)
+            {
+                case ItemType.Drink:
+                    DrinkItems.Add((Drink) item);
+                    break;
+                case ItemType.Food:
+                    FoodItems.Add((Food) item);
+                    break;
+                case ItemType.Weapon:
+                    WeaponItems.Add((Weapon) item);
+                    break;
+            }
+        }
+
+        public void AddItem(List<VMItem> items)
+        {
+            foreach (var item in items)
+            {
+                AddItem(item);
+            }
+        }
+    }
+}
