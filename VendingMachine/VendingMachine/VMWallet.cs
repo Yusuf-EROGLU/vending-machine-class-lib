0e3e37812c09d18a284fcbd47f23e8a5fb2af692
yusuf <yusuf.eroglu@cresaurteam.com>
2021-06-30 02:10:12 +0300

    add wallet

---
+++ VendingMachine/VendingMachine/VMWallet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public class VMWallet : IVendingMachineWallet
+    {
+        public List<Coin> CoinsInSafe;
+        public List<Coin> PendingCoins;
+
+        public void SetCurrency(List<Coin> initialCurrency)
+        {
+            CoinsInSafe.AddRange(initialCurrency);
+        }
+    }
+}
