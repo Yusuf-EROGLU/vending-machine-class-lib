eaf9c6bcbe65661955a88fca2b09e2411002d41d
yusuf <yusuf.eroglu@cresaurteam.com>
2021-06-30 02:09:08 +0300

    add catalogue

---
+++ VendingMachine/VendingMachine/Catalogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    /// <summary>
+    /// Catalogue that can define group of ıtem can be used for quick setup of Vending Machine
+    /// </summary>
+    public struct Catalogue
+    {
+        public List<Drink> DrinkItems { get; set; }
+        public List<Food> FoodItems { get; set; }
+        public List<Weapon> WeaponItems { get; set; }
+        public int Id { get; set; }
+        
+    }
+}
