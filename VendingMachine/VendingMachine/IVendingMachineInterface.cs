c895a2ce868f01ca2b565b118117d3f425990984
yusuf <yusuf.eroglu@cresaurteam.com>
2021-06-30 02:10:38 +0300

    add vminterface

---
+++ VendingMachine/VendingMachine/IVendingMachineInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public interface IVendingMachineInterface
+    {
+        void InitializeInterface();
+        void InsertCoin(Coin coin);
+
+        void InsertCoin(List<Coin> coin);
+
+        void BuyItem(int itemId);
+
+        void PriceInquiry();
+    }
+}
---
+++ VendingMachine/VendingMachine/VMInterface.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public class VMInterface : IVendingMachineInterface
+    {
+        public IVendingMachineInventory Inventory { get; set; }
+        public IVendingMachineWallet Wallet { get; set; }
+
+        public void InitializeInterface()
+        {
+        }
+
+        public void InsertCoin(Coin coin)
+        {
+        }
+
+        public void InsertCoin(List<Coin> coin)
+        {
+        }
+
+        public void BuyItem(int itemId)
+        {
+        }
+
+        public void PriceInquiry()
+        {
+        }
+    }
+}
